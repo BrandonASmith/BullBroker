5ccfdc566edcb283361ead7ddd85b93473f4daf8
BrandonASmith <basmith808@gmail.com>
2025-07-10 15:12:19 -0400

    Update ai_engine.py

--- ai_engine.py
+++ ai_engine.py
@@ -10,10 +10,31 @@ if not OPENAI_API_KEY:
 openai.api_key = OPENAI_API_KEY
 
 # Sample high-quality tickers — you can extend or fetch dynamically
-TICKER_LIST = [
-    "AAPL", "MSFT", "NVDA", "TSLA", "AMD", "AMZN", "GOOGL", "META", "NFLX",
-    "SMCI", "PLTR", "AVGO", "SHOP", "U", "ENPH", "SOFI", "RIVN", "CROX",
-    "NIO", "F", "XOM", "LULU", "BX", "DKNG", "T", "INTC", "BABA"
+TRACKER_LIST = [
+    "AAPL", "MSFT", "NVDA", "GOOGL", "AMZN", "META", "TSLA", "AVGO", "BRK.B", "LLY",
+    "UNH", "V", "JPM", "XOM", "JNJ", "WMT", "MA", "PG", "HD", "COST", "MRK", "CVX", "ABBV", "PEP", "ADBE",
+    "CRM", "BAC", "NFLX", "KO", "TMO", "AMD", "DIS", "ABT", "PFE", "MCD", "NKE", "LIN", "INTC", "ORCL",
+    "WFC", "ACN", "DHR", "MS", "TXN", "AMAT", "NEE", "QCOM", "PM", "GE", "BMY", "UPS", "CAT", "IBM", "RTX",
+    "SPGI", "GS", "BLK", "ISRG", "MDT", "SYK", "HON", "ELV", "CI", "NOW", "REGN", "MU", "VRTX", "PLD",
+    "LMT", "ADI", "ADP", "T", "DE", "ZTS", "MMC", "SCHW", "CB", "ETN", "EQIX", "F", "MO", "USB", "CSCO",
+    "AXP", "BDX", "C", "DUK", "NSC", "SO", "FDX", "APD", "EOG", "GM", "CL", "ADSK", "ROST", "TJX", "SBUX",
+    "SLB", "GILD", "D", "EMR", "PSA", "MCO", "NOC", "VLO", "TRV", "TT", "AON", "PCAR", "LRCX", "HCA", "AEP",
+    "PH", "IDXX", "EXC", "KMB", "HUM", "ROK", "MTD", "KMI", "DVN", "PAYX", "FTNT", "WELL", "ED", "AIG", "AME",
+    "IQV", "MAR", "SPG", "DAL", "CTAS", "KLAC", "LHX", "FIS", "DLR", "AFL", "VTR", "HES", "AZO", "HAL", "O",
+    "FAST", "PWR", "PEG", "ADM", "YUM", "ODFL", "RMD", "MKC", "CEG", "HPQ", "STZ", "ALGN", "RCL", "TSCO",
+    "EQR", "CHD", "AWK", "WBA", "EIX", "BALL", "DRI", "NUE", "NDAQ", "KEYS", "CTSH", "SRE", "MTCH", "EXPE",
+    "ULTA", "MNST", "COF", "ATO", "VRSK", "AVB", "FE", "XYL", "TER", "CF", "TYL", "INVH", "IFF", "SBAC",
+    "HSY", "LUV", "ZBRA", "MPWR", "POOL", "CDNS", "FSLR", "ENPH", "RUN", "NEE", "SEDG", "CSIQ", "BLNK", "CHPT",
+    "PLTR", "SNOW", "U", "DDOG", "ZS", "OKTA", "NET", "PATH", "AI", "BILL", "TWLO", "AFRM", "DOCN", "UPST",
+    "SOFI", "RIVN", "LCID", "NKLA", "HOOD", "CROX", "SHOP", "SQ", "PYPL", "BABA", "JD", "PDD", "LI", "NIO",
+    "TSM", "ASML", "INTU", "WM", "TGT", "KR", "LOW", "BBY", "VFC", "DELL", "HP", "Z", "EBAY", "YELP", "GRMN",
+    "CZR", "MGM", "WYNN", "TTWO", "EA", "ATVI", "ROKU", "DKNG", "PENN", "LYV", "BIDU", "ARCC", "MAIN", "BX",
+    "KHC", "GIS", "HSBC", "ING", "BP", "TOT", "RY", "TD", "ENB", "BNS", "BMO", "NSRGY", "SNY", "GSK", "UL",
+    "BTI", "NGG", "RELX", "WBK", "TEF", "VOD", "RIO", "VALE", "GOLD", "FCX", "NEM", "MOS", "SMG", "ADM",
+    "BG", "CTVA", "NTR", "CF", "CARG", "APP", "WING", "TTD", "NVCR", "ON", "SWKS", "QRVO", "HUBS", "FICO",
+    "TEAM", "MNDY", "TXG", "BRZE", "SPLK", "ESTC", "MDB", "GLBE", "FIVN", "GTLB", "ZS", "ROKU", "FIGS",
+    "RVLV", "LEVI", "DKS", "CHWY", "PTON", "NU", "MELI", "WBD", "PARA", "DISCA", "MTN", "LTH", "XPO", "FWRD",
+    "R", "TTC", "IR", "WMS", "PNR", "AWI", "MAS", "LEG", "WHR", "LII", "AOS", "OC", "BWA", "ALSN"
 ]
 
 # Map keywords to pick types
